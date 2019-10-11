5d0d5c767323b14f32e2e916af5c59e69ad6d3c1
varma1096 <rajivvarma.ca@gmail.com>
2019-10-11 04:23:12 +0000

    second commit

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var express = require('express');
+require('dotenv').config();
 var app = express();
 
 app.get('/',function (req,res){
 	res.send('hello rajiv');
 });
-
-app.listen(8081,function(){
+console.log('process.env.PORT' , process.env.PORT);
+app.listen(process.env.PORT,function(){
 	console.log('Example app listening on port 3000!');
 });
 
